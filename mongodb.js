12f15664ab6dfa81032363228babcc1872e10209
Ibrahim Hamdy  Ramadan <ibrahimhamdyramadan@192.168.1.8>
2021-11-12 19:10:39 +0200

    First commit

---
+++ mongodb.js
@@ -0,0 +1,23 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const connectionURL = "mongodb://localhost:27017";
+const databaseName = 'task-manager';
+
+
+MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
+    if (error) {
+        return console.log("Unable to connect to database!");
+    }
+
+    const db = client.db(databaseName);
+
+    db.collection('users').insertOne({
+        name: 'hema',
+        age : '26',
+    })
+    db.collection('users').insertOne({
+        name: 'ahmed',
+        age : '26',
+    })
+});
