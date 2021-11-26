04baa6cb932ef04f56c4d88b8292e01261c763b1
Ibrahim hamdy <mslm356@outlook.com>
2021-11-26 16:19:20 +0200

    Add the users model and route.

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,57 +6,6 @@ mongoose.connect("mongodb://localhost:27017/task-manger-api", {
     useCreateIndex: true,
 });
 
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if(!validator.isEmail(value)) {
-                throw new Error('Email is invalid');
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 7,
-        trim: true,
-        validate(value) {
-            if(value.toLowerCase().includes("password") ) {
-                throw new Error('Password can not contain "Password"');
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if(value < 0 ) {
-                throw new Error('Age most be a positive number');
-            }
-        }
-    }
-});
-
-const me = new User({
-    name: "hema",
-    email: "mslm356@gmail.com",
-    password: "     aaaaaaaaaaaaa    ",
-})
-
-me.save().then((result) => {
-    console.log(result);
-}).catch((error)=> {
-    console.log(error);
-});
-
 const Task = mongoose.model('Task', {
     description: {
         type: String,
